2429ea430a2245a162c8347e54e77700892c5a7b
osita kizito <ositakizito4@gmail.com>
2026-01-17 09:16:21 +0100

    first commit

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10476337
+    "domain": "3nbf4.com",
+    "zoneId": 10476588
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
